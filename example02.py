4e8496faa2a3033d78d5582444c037f9159f3815
johnshea <shea.john@mac.com>
2021-11-08 08:13:13 -0500

    update of repo

---
+++ example02.py
@@ -0,0 +1,8 @@
+#import code as a library lsample.py
+import lsample
+
+# Using dot notation reference the imported object
+lsample.welcome()
+
+
+
---
+++ lsample.py
@@ -0,0 +1,4 @@
+#define a function to be imported. This is very similar to using the Meraki SDK.
+# We can write code for reuse and import it into our projects
+def welcome():
+    print("Welcome to Meraki as a platform")
