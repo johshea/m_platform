2d47a5adc9385c07f3e7657adf93e550c86dd4c7
johnshea <shea.john@mac.com>
2021-11-08 09:22:33 -0500

    update of repo

---
+++ example05.py
@@ -0,0 +1,18 @@
+# import the CSV Library
+import csv
+
+# open and read the file
+with open("csample.csv")as csvfile:
+    csample = csv.reader(csvfile, delimiter=',')
+
+
+# get the rows of the CSV file and show them
+    for row in csample:
+        print("{device} is in {location} and has IP {ip}.".format(
+            device=row[0],
+            ip=row[1],
+            location=row[1])
+        )
+
+
+
